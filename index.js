43a0990b0948712e33eb3992420c4f85ea076cad
carlosgutierrez121 <134764909+carlosgutierrez121@users.noreply.github.com>
2024-03-31 02:53:41 +0800

    Added_Sql_connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql');
 const dotenv = require('dotenv');
 const express = require('express');
 const hbs = require('hbs');
@@ -15,6 +16,22 @@ app.use(express.static('public'));
 app.use(`/`, function(req, res) {
     res.render('index');
 });
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Stadvdb35_server0',
+    database: 'stadvdb_mco2'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database: ', err);
+        return;
+    }
+    console.log('Connected to database.');
+    connection.end();
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}`);
