74c8b78867847bbd7a841f1737a106ae12fcfe39
carlosgutierrez121 <134764909+carlosgutierrez121@users.noreply.github.com>
2024-04-08 06:34:55 +0800

    Added_pool_connections

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 const express = require('express');
 const hbs = require('hbs');
+const Sequelize = require('sequelize');
 
 const app = express();
 
@@ -17,26 +18,53 @@ app.use(`/`, function(req, res) {
     res.render('index');
 });
 
-//connection for server0 (central node)
-const server0 = mysql.createConnection({
-    host: process.env.SERVER0_HOST,
-    user: process.env.SERVER0_USER,
-    password: process.env.SERVER0_PASS,
-    database: 'stadvdb_mco2'
-});
-
-server0.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database: ', err);
-        return;
+//sample connection pool (mysql workbench)
+const sample_sql = new Sequelize(
+    'stadvdb_mco2',
+    'root',
+    'admin',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
     }
-    console.log('Connected to database.');
-    server0.end();
-});
+);
 
-// const server1 = mysql.createConnection({
-//     host: ''
-// })
+//connection pool for node1 (central node)
+const node1 = new Sequelize(
+    'stadvdb_mco2',
+    process.env.SERVER0_USER,
+    process.env.SERVER0_PASS,
+    {
+        host: process.env.SERVER0_HOST,
+        dialect: 'mysql'
+    }
+);
+
+const node2 = new Sequelize(
+    'stadvdb_mco2',
+    process.env.SERVER1_USER,
+    process.env.SERVER1_PASS,
+    {
+        host: process.env.SERVER1_HOST,
+        dialect: 'mysql'
+    }
+);
+
+const node3 = new Sequelize(
+    'stadvdb_mco2',
+    process.env.SERVER2_USER,
+    process.env.SERVER2_PASS,
+    {
+        host: process.env.SERVER2_HOST,
+        dialect: 'mysql'
+    }
+);
+
+sample_sql.authenticate().then(() => {
+    console.log('Connection to Node 1 server has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to Node 1 server: ', error);
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}`);
