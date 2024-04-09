5cbfeb6c77688d26c470d9b8142cbf82916ae186
carlosgutierrez121 <134764909+carlosgutierrez121@users.noreply.github.com>
2024-04-09 14:38:24 +0800

    AddedRoutes

---
+++ controllers/controller.js
@@ -0,0 +1,26 @@
+const controller =  {
+    getFavicon: function(req, res){
+        res.status(204);
+    },
+
+    getIndex: function(req, res){
+        res.render('index');
+    },
+
+    //CRUD
+    getAppts: function(req, res){
+        const node1 = req.node1;
+        const node2 = req.node2;
+        const node3 = req.node3;
+
+        node2.query ("SELECT count(apptid) FROM appt")
+            .then((results) => {
+                res.json(results);
+            })
+            .catch((err) => {
+                res.status(500).sent('Error');
+            });
+    }
+};
+
+module.exports = controller;
